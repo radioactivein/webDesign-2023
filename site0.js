aaad826e513094b9be974d455b700b46a23d38c3
Al Ameen <78306701+radioactivein@users.noreply.github.com>
2023-05-09 16:06:38 +0530

    custom animations

--- site0.js
+++ site0.js
@@ -1,4 +1,4 @@
-const button =document.querySelector('bt1');
+const button =document.querySelector('button');
 button.addEventListener('click', ()=>{
     button.classList.toggle('party-time')
 } );
